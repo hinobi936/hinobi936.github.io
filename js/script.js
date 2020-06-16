573233f62506cab2907c6756b2f73c026eea5218
hinobi936 <hi-nobi-9@mail.ru>
2020-06-17 01:58:39 +0500

    to check

--- js/script.js
+++ js/script.js
@@ -28,24 +28,25 @@ document.addEventListener('DOMContentLoaded', () => {
         // теплоемкость крышки и подложки соответственно
         cp_kVal, cp_pVal,
         // плотность крышки, подложки и углепластика соответственно
-        ro_kVal, ro_pVal, ro_tzpVal;
+        ro_kVal, ro_pVal, ro_tzpVal,
+        // число итераций
+        N;
 
-    const tempVal = {}
+    // переменные для хранения значений теплопроводности и теплоемкости углепластика
+    let cp_mc = [], lambda_mc = [];
 
-    // let lambdaVal, cpVal, roVal, timeVal, edgeLeftVal, edgeRightVal,
-    //     N, K, materialWidthVal, firstTimeVal;
+    // для записи данных времени и температур из таблицы измеренных температур
+    const tempVal = {};
+    let time = [], tempK = [], tempP = [], tempTZP = [],
+        bk = [], bp = [], bmc = [], tk, tp, c1 = [], c2 = [], tx_tau = [];
+
+    // для рисования графика
 
     let myCanvas = document.getElementById("graf");
     let ctx = myCanvas.getContext("2d");
 
-    let width_layer, time_layer, calculateData;
+    let calculateData;
     calculateData = document.createElement('p');
-
-    // двумерный массив данных для температур
-    let T = [];
-    let Txy = []; //для canvas
-    // одномерные массивы 
-    let alfa = [0], beta = [0], A = [0], B = [0], C = [0], F = [0];
 
     document.body.addEventListener('input', (e) => {
         element = e.target.tagName;
@@ -113,32 +114,30 @@ document.addEventListener('DOMContentLoaded', () => {
                 // console.log(cells[k].children[0].value)
             }
         }
-        console.log(tempVal)
+        // console.log(tempVal)
     }
     // расчет теплопроводности и теплоемкости
     function createMasCharacteristic(massiv) {
-        // определяем количество строк в таблице
-        let N = Object.keys(massiv).length;
+        // определяем количество строк в таблице (N - количество строк)
+        N = Object.keys(massiv).length;
         // console.log(Object.keys(massiv).length-1);
-        let time = [], tempK = [], tempP = [], tempTZP = [],
-            bk, bp, bmc, tk, tp, tx_tau;
 
-        // заполняем массивы time, tempK, tempP, tempTZP
+        // заполняем массивы time, tempK, tempP, tempTZP значениями из таблицы
         for (let i = 0; i < N; i++) {
-            time[i] = massiv[i + 1].time;
-            tempK[i] = massiv[i + 1].tempK;
-            tempP[i] = massiv[i + 1].tempP;
-            tempTZP[i] = massiv[i + 1].tempTZP;
+            time[i] = Number(massiv[i + 1].time);
+            tempK[i] = Number(massiv[i + 1].tempK);
+            tempP[i] = Number(massiv[i + 1].tempP);
+            tempTZP[i] = Number(massiv[i + 1].tempTZP);
         }
 
         //рассчет данных
         for (let i = 0; i < N - 1; i++) {
             // расчет коэффициентов bk, bp, bmc
             //toFixed - округление до 3 точки после запятой
-            bk = ((tempK[i + 1] - tempK[i]) / Math.sqrt(time[i + 1] - time[i])).toFixed(3);
-            bp = ((tempP[i + 1] - tempP[i]) / Math.sqrt(time[i + 1] - time[i])).toFixed(3);
-            bmc = ((tempTZP[i + 1] - tempTZP[i]) / Math.sqrt(time[i + 1] - time[i])).toFixed(3);
-            console.log(bk, bmc, bp);
+            bk[i] = Number(((tempK[i + 1] - tempK[i]) / Math.sqrt(time[i + 1] - time[i])).toFixed(3));
+            bp[i] = Number(((tempP[i + 1] - tempP[i]) / Math.sqrt(time[i + 1] - time[i])).toFixed(3));
+            bmc[i] = Number(((tempTZP[i + 1] - tempTZP[i]) / Math.sqrt(time[i + 1] - time[i])).toFixed(3));
+            // console.log(bk, bmc, bp);
 
             let a = lambda_kVal / (cp_kVal * ro_kVal);
             // число Фурье при всех итерациях в расчете вышло меньше 0.004 
@@ -146,18 +145,56 @@ document.addEventListener('DOMContentLoaded', () => {
             console.log(Fo);
 
             // расчет констант с1 и с2 для квадратного уравнения
-            let c1 = ((tempK[i] - tempP[i]) * Math.pow(thick_XmcVal, 2) -
+            c1[i] = ((tempK[i] - tempP[i]) * Math.pow(thick_XmcVal, 2) -
                 (tempK[i] - tempTZP[i]) * Math.pow(thick_tzpVal, 2)) /
                 (thick_tzpVal * thick_XmcVal * (thick_tzpVal - thick_XmcVal));
-            console.log(c1);
-            let c2 = ((tempK[i] - tempTZP[i]) * thick_tzpVal - (tempK[i] - tempP[i]) * thick_XmcVal) /
+            // console.log(c1);
+            c1[i] = c1[i].toFixed(3)
+            c2[i] = ((tempK[i] - tempTZP[i]) * thick_tzpVal - (tempK[i] - tempP[i]) * thick_XmcVal) /
                 (thick_tzpVal * thick_XmcVal * (thick_tzpVal - thick_XmcVal));
-            console.log(c2);
+            c2[i] = c2[i].toFixed(3)
+
+            // console.log(c2);
 
             // рассчитываем интеграл
-            tx_tau = tempTZP[i] * thick_tzpVal +
-                c1 * (Math.pow(thick_tzpVal, 2) / 2) + c2 * (Math.pow(thick_tzpVal, 3) / 3);
-            console.log(tx_tau);
+            tx_tau[i] = tempTZP[i] * thick_tzpVal +
+                c1[i] * (Math.pow(thick_tzpVal, 2) / 2) + c2[i] * (Math.pow(thick_tzpVal, 3) / 3);
+            tx_tau[i] = tx_tau[i].toFixed(3)
+
+            // console.log(tx_tau);
+
+            // рассчет теплоемкости cp[i]
+            console.log(
+                tempK[i + 1],
+                Math.sqrt(Math.PI),
+                bk,
+                bp,
+                time[i + 1],
+                time[i],
+                thick_tzpVal,
+                ro_tzpVal,
+                Math.sqrt(ro_tzpVal),
+                lambda_kVal,
+                cp_kVal,
+                Math.sqrt(lambda_kVal / cp_kVal),
+                Math.sqrt(cp_kVal / lambda_kVal),
+                Math.sqrt(Math.PI)
+
+            );
+
+            let equal = (tempK[i + 1] - ((Math.sqrt(Math.PI)) * (bk[i] + bp[i]) * (time[i + 1] - time[i])
+                / 2 * thick_tzpVal * Math.sqrt(ro_tzpVal)) * (Math.sqrt(lambda_kVal / cp_kVal)) +
+                (1 / 3) * (bk[i] - bp[i] / 2) * (Math.sqrt(cp_kVal / lambda_kVal)));
+
+            let chislitel = (cp_kVal * ro_kVal * thick_kVal * tempK[i] +
+                cp_pVal * ro_pVal * thick_pVal * tempP[i]) -
+                (cp_kVal * ro_kVal * thick_kVal + cp_pVal * ro_pVal * thick_pVal * equal);
+
+            let znamenatel = ro_kVal * thick_tzpVal * equal - ro_tzpVal * tx_tau[i];
+            cp_mc[i] = (chislitel / znamenatel);
+            console.log(equal, chislitel, znamenatel, cp_mc);
+            cp_mc[i]=cp_mc[i].toFixed(3)
+
 
         }
     };
@@ -180,29 +217,24 @@ document.addEventListener('DOMContentLoaded', () => {
 
         // вывод массива на страницу 2й способ
         let table = document.createElement('table');
-        for (let i = 0; i <= K + 1; i++) {
+        for (let i = 0; i < N - 1; i++) {
             let tr = document.createElement('tr');
             let th = document.createElement('th');
-            th.setAttribute('colspan', N + 2);
             th.style.backgroundColor = 'lightgray';
-            th.innerHTML = `<hr>ШАГ по времени = ${i}<hr>`;
+            th.innerHTML = `<hr> <em>i = ${i}</em><hr>`;
             tr.appendChild(th);
             table.appendChild(tr);
 
-            tr = document.createElement('tr');
-            for (let j = 0; j <= N + 1; j++) {
-                let th = document.createElement('th');
-                th.innerHTML = ` СЛОЙ ${j}`;
-                tr.appendChild(th);
-            }
-            table.appendChild(tr);
 
             tr = document.createElement('tr');
-            for (let j = 0; j <= N + 1; j++) {
-                let td = document.createElement('td');
-                td.innerHTML = T[j][i].toFixed(3);
-                tr.appendChild(td);
-            }
+            let td = document.createElement('td');
+            td.innerHTML = `<em> bk = ${bk[i]}, bp = ${bp[i]}, bmc = ${bmc[i]}, <br>
+            c1 = ${c1[i]}, c2 = ${c2[i]}, <br>
+            tx_tau = ${tx_tau[i]}, <br>
+            теплоемкость cp_mc = ${cp_mc[i]} </em>`
+
+            tr.appendChild(td);
+
             table.appendChild(tr);
         }
         dataOut.appendChild(table);
@@ -243,12 +275,12 @@ document.addEventListener('DOMContentLoaded', () => {
         ctx.stroke();
     };
 
-    calculateBtn.addEventListener('click', () => {
-        event.preventDefault();
+    calculateBtn.addEventListener('click', (e) => {
+        e.preventDefault();
         updateValues();
         readTemperature()
         createMasCharacteristic(tempVal);
-        // printMas();
+        printMas();
         // printGraf();
     })
 
